675a41798eb750feb19c0e78b625aa66d4823316
francoragout <francoragout1@gmail.com>
2023-07-25 03:31:34 -0300

    some details were checked

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='response text-bg-dark mt-auto d-flex bg-gradient mx-auto' style={{height: '56px'}}>
-      <p className='mx-auto my-auto'>Copyright <i class="bi bi-cc-circle"></i> 2023, Finance App. All rights reserved.</p>
+      <p className='mx-auto my-auto'>Copyright <i className="bi bi-cc-circle"></i> 2023, Finance App. All rights reserved.</p>
     </div>
   )
 }
